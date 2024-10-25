e429bb6eed5a377a2126a3e4c83b8e1e977e2083
Dante4K18 <dan@dan>
2024-10-25 17:25:09 +0300

    completed-lab

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,19 @@
 class MySet:
+    def __init__(self, iterable=None):
+        self.dictionary = {}  # Using a dictionary to simulate set behavior
+        if iterable is not None:
+            for item in iterable:
+                self.add(item)  # Add items to the set
 
-    pass
+    def add(self, value):
+        self.dictionary[value] = True  # Use dictionary keys for unique values
+
+    def delete(self, value):
+        if value in self.dictionary:
+            del self.dictionary[value]
+
+    def has(self, value):
+        return value in self.dictionary
+
+    def size(self):
+        return len(self.dictionary)
